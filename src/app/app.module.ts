4fa742fc40a0a87c2987eeb016dcc936fe7f7f83
Raineer <delaritaraineer81@gmail.com>
2018-01-23 13:36:25 +0800

    product.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
-
+import { HttpClientModule } from "@angular/common/http";
+import { StoreModule } from "@ngrx/store";
 import { AppComponent } from './app.component';
 
 
---
+++ src/app/effects/product.effects.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@angular/core";
+import { Action } from "@ngrx/store";
+import { Actions, Effect } from "@ngrx/effects";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/switchMap";
+import "rxjs/add/operator/mergeMap";
+import "rxjs/add/operator/catch";
+import "rxjs/add/operator/debounceTime";
+import { of } from "rxjs/observable/of";
